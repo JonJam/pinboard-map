d6308dfe01d3d7b3851550043ef30f7dba14b3c3
Jonathan Harrison <jonjam1990@googlemail.com>
2019-01-14 20:58:44 +0000

    Performance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+// Set root project name. See https://docs.gradle.org/current/userguide/intro_multi_project_builds.html#sec:structure_of_a_multiproject_build
 rootProject.name = "pinboard-map"
 
 // https://docs.gradle.org/current/userguide/multi_project_builds.html

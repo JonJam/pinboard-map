6692bb26f4e10b266d7c8996147ece1623c2836f
Jonathan Harrison <jonjam1990@googlemail.com>
2019-01-15 20:31:51 +0000

    Switched to Jcenter and use dependency constraints

--- services/location-service/location-svc/build.gradle.kts
+++ services/location-service/location-svc/build.gradle.kts
@@ -3,15 +3,26 @@ plugins {
 }
 
 repositories {
-    mavenCentral()
+    jcenter()
+}
+
+// Use dependency constraints. See https://docs.gradle.org/current/userguide/declaring_dependencies.html#declaring_a_dependency_without_version
+dependencies {
+    constraints {
+        implementation("org.glassfish.jersey.containers:jersey-container-grizzly2-http:2.27")
+        implementation("org.glassfish.jersey.inject:jersey-hk2:2.27")
+        implementation("org.glassfish.jersey.media:jersey-media-json-jackson:2.27")
+        implementation("org.glassfish.hk2:guice-bridge:2.5.0-b62")
+        testImplementation("junit:junit:4.9")
+    }
 }
 
 dependencies {
-    implementation("org.glassfish.jersey.containers:jersey-container-grizzly2-http:2.27")
-    implementation("org.glassfish.jersey.inject:jersey-hk2:2.27")
-    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:2.27")
-    implementation("org.glassfish.hk2:guice-bridge:2.5.0-b62")
-    testImplementation("junit:junit:4.9")
+    implementation("org.glassfish.jersey.containers:jersey-container-grizzly2-http")
+    implementation("org.glassfish.jersey.inject:jersey-hk2")
+    implementation("org.glassfish.jersey.media:jersey-media-json-jackson")
+    implementation("org.glassfish.hk2:guice-bridge")
+    testImplementation("junit:junit")
 }
 
 // Performance suggestions for Java Projects. See https://guides.gradle.org/performance/#suggestions_for_java_projects

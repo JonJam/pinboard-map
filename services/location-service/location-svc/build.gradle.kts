627b6ed7231436852eae170acb4866fc6e544a6e
Jonathan Harrison <jonjam1990@googlemail.com>
2019-01-15 20:10:33 +0000

    Performance suggestions for java projects

--- services/location-service/location-svc/build.gradle.kts
+++ services/location-service/location-svc/build.gradle.kts
@@ -13,3 +13,21 @@ dependencies {
     implementation("org.glassfish.hk2:guice-bridge:2.5.0-b62")
     testImplementation("junit:junit:4.9")
 }
+
+// Performance suggestions for Java Projects. See https://guides.gradle.org/performance/#suggestions_for_java_projects
+tasks.withType<JavaCompile> {
+    options.isFork = true
+}
+
+tasks.withType<Test> {
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+}
+
+tasks.withType<Test> {
+    setForkEvery(100)
+}
+
+tasks.withType<Test> {
+    reports.html.isEnabled = false
+    reports.junitXml.isEnabled = false
+}

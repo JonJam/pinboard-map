7b60d8851d787fde31a878f21e0fed7ad6723adc
Jonathan Harrison <jonjam1990@googlemail.com>
2019-02-04 19:33:02 +0000

    Removed tomcat plugin, created dockerfile

--- services/location-service/location-svc/src/main/java/com/jonjam/pinboard/service/location/ExampleRequest.java
+++ services/location-service/location-svc/src/main/java/com/jonjam/pinboard/service/location/ExampleRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.jonjam.pinboard.common.objectmodel.Immutable;
 
 @Immutable
-// TODO Work out so do not need to specify this
 @JsonDeserialize(builder = ExampleRequest.Builder.class)
 public abstract class ExampleRequest {
   public abstract String getBar();

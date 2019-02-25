e256ed2102851fce304db0b08417567736d0ee7c
Jonathan Harrison <jonjam1990@googlemail.com>
2019-02-25 20:49:19 +0000

    Java remote debug

--- services/location-service/docker/setenv.sh
+++ services/location-service/docker/setenv.sh
@@ -3,3 +3,6 @@ CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=7035"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+
+# Enabling Java Remote Debug (https://wiki.apache.org/tomcat/FAQ/Developing#Q1)
+CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:4035,server=y,suspend=n"

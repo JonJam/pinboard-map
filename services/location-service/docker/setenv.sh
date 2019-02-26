750f997c5f9422f046b6f3961abe76c1c5f13211
Jonathan Harrison <jonjam1990@googlemail.com>
2019-02-26 19:24:57 +0000

    Working docker-compose file

--- services/location-service/docker/setenv.sh
+++ services/location-service/docker/setenv.sh
@@ -1,8 +1,8 @@
 # Enabling JMX Remote (https://tomcat.apache.org/tomcat-9.0-doc/monitoring.html#Enabling_JMX_Remote)
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote"
-CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=7035"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=7031"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
 
 # Enabling Java Remote Debug (https://wiki.apache.org/tomcat/FAQ/Developing#Q1)
-CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:4035,server=y,suspend=n"
+CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:4031,server=y,suspend=n"

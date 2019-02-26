f472ec14b363a5013cd30e03572a2aa9105e8229
Jonathan Harrison <jonjam1990@googlemail.com>
2019-02-26 20:46:59 +0000

    Ports set using APP_ID environment variable

--- services/location-service/docker/tomcat/dev/setenv.sh
+++ services/location-service/docker/tomcat/dev/setenv.sh
@@ -1,8 +1,11 @@
+CATALINA_OPTS="$CATALINA_OPTS -Dport.http=80${APP_ID}"
+CATALINA_OPTS="$CATALINA_OPTS -Dport.https=84${APP_ID}"
+
 # Enabling JMX Remote (https://tomcat.apache.org/tomcat-9.0-doc/monitoring.html#Enabling_JMX_Remote)
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote"
-CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=7031"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=70${APP_ID}"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
 
 # Enabling Java Remote Debug (https://wiki.apache.org/tomcat/FAQ/Developing#Q1)
-CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:4031,server=y,suspend=n"
+CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:40${APP_ID},server=y,suspend=n"

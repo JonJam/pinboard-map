7272d0c681209a005ae81f367d824953340afd2c
Jonathan Harrison <jonjam1990@googlemail.com>
2019-07-11 21:14:36 +0100

    Added parameter names module back and fixed pinboard-map-web

--- common/common-objectmodel/src/main/java/com/jonjam/pinboard/common/objectmodel/ObjectMapperBuilder.java
+++ common/common-objectmodel/src/main/java/com/jonjam/pinboard/common/objectmodel/ObjectMapperBuilder.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 public class ObjectMapperBuilder {
 
@@ -20,7 +21,8 @@ public class ObjectMapperBuilder {
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         .configure(SerializationFeature.WRITE_DATES_WITH_ZONE_ID, true)
         .registerModule(new JavaTimeModule())
-        .registerModule(new Jdk8Module());
+        .registerModule(new Jdk8Module())
+        .registerModule(new ParameterNamesModule());
   }
 }
 

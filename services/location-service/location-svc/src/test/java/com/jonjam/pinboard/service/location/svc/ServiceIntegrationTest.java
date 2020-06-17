0da8d07dd444b273dcb8d941bb011a48190a0edb
Jonathan Harrison <jonjam1990@googlemail.com>
2020-06-17 21:13:44 +0100

    Fixed integration test

--- services/location-service/location-svc/src/test/java/com/jonjam/pinboard/service/location/svc/ServiceIntegrationTest.java
+++ services/location-service/location-svc/src/test/java/com/jonjam/pinboard/service/location/svc/ServiceIntegrationTest.java
@@ -11,7 +11,6 @@ import feign.slf4j.Slf4jLogger;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,7 +23,9 @@ public abstract class ServiceIntegrationTest {
   private static final String APP_ID_BUILD_ARGUMENT_NAME = "APP_ID";
   private static final String TEST_APP_ID = "99";
   private static final String VERSION_BUILD_ARGUMENT_NAME = "VERSION";
-  private static final String VERSION = "prod";
+  private static final String VERSION = "dev";
+  private static final String ENVIRONMENT_VARIABLE = "ENVIRONMENT";
+  private static final String ENVIRONMENT = "dev";
 
   // NOTE - last two digits map to TEST_APP_ID.
   private static final int HTTP_PORT = 8099;
@@ -33,8 +34,9 @@ public abstract class ServiceIntegrationTest {
   private static GenericContainer<?> serviceContainer = new GenericContainer(new ImageFromDockerfile()
       .withFileFromPath(".", DOCKER_BUILD_CONTEXT)
       .withBuildArg(APP_ID_BUILD_ARGUMENT_NAME, TEST_APP_ID)
-      .withBuildArg(VERSION_BUILD_ARGUMENT_NAME, VERSION)
-  ).withExposedPorts(HTTP_PORT);
+      .withBuildArg(VERSION_BUILD_ARGUMENT_NAME, VERSION))
+      .withExposedPorts(HTTP_PORT)
+      .withEnv(ENVIRONMENT_VARIABLE, ENVIRONMENT);
 
   protected <T> T getClient(final Class<T> serviceInterface) {
     final String address = "http://"
